6926e52c5138e6767b18dcaae33c5ebdb1c550ab
Rakhina <rakhina@bdd.local>
2021-12-22 13:18:09 +0300

    авторизация

--- v_pohod/menu/views.py
+++ v_pohod/menu/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class MainView(APIView):
+    permission_classes = [IsAuthenticated, ]
+
+    def post(self, request):
+        context = {
+            'error': '',
+            'info': '',
+        }
+
+        return render(request, 'templates/main.html', context)
+
+
+    def get(self, request):
+        context = {
+            'error': '',
+            'info': '',
+        }
+
+        return render(request, 'templates/main.html', context)
