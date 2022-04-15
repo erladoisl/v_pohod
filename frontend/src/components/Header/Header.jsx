fa956a326c28392124c3539ecbc47abcc2f02419
Rakhina Latypova <rakhina.bdd@gmail.com>
2022-04-15 14:46:18 +0300

    logOUT

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,33 +1,44 @@
+import { NavLink } from "react-router-dom";
+import { useContext } from 'react'
+import { UserContext } from "../../contexts/index"
+
 function Header() {
-    return (
-        <header>
-  <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="#">Carousel</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarCollapse">
-        <ul className="navbar-nav me-auto mb-2 mb-md-0">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link disabled">Disabled</a>
-          </li>
-        </ul>
-        <form className="d-flex">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
-      </div>
-    </div>
-  </nav>
-</header>
-    );
+  const [state, dispatch] = useContext(UserContext)
+  console.log(state)
+
+  const logOut = (() => {
+    dispatch({ 'type': 'authorization', 'user': undefined })
+  });
+  return (
+    <header>
+      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">ВПоход</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <ul className="navbar-nav me-auto mb-2 mb-md-0">
+              <li className="nav-item">
+                <NavLink to='/' className="nav-link ">
+                  Главная
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to='/raskladki' className="nav-link ">
+                  Архив Раскладок
+                </NavLink>
+              </li>
+            </ul>
+            <div className="ms-auto link-light" >
+              <span className="d-lg-inline-block my-2 my-md-0 ms-md-3 text-white" >{state.user.name}</span>
+              <button className="btn btn-dark d-lg-inline-block my-2 my-md-0 ms-md-3 text-white" onClick={logOut}>Выйти</button>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
