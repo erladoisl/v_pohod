45d0377eeb4fc596027d324d8fe7ec2a6f3080e0
Rakhina Latypova <rakhina.bdd@gmail.com>
2022-04-19 08:43:47 +0300

    Авторизация и регистрация перенесена в приложение user

--- v_pohod/user/urls.py
+++ v_pohod/user/urls.py
@@ -13,13 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include
-from django.contrib import admin
 from django.urls import path
+from user.views import CustomAuthToken
+from user.views import CustomRegistrationToken
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', include('menu.urls')),
-    path('', include('user.urls'))
+    path('api-token-auth/', CustomAuthToken.as_view()),
+    path('api/registration/', CustomRegistrationToken.as_view()),
 ]
--- v_pohod/user/views.py
+++ v_pohod/user/views.py
@@ -3,7 +3,6 @@ import traceback
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from django.contrib.auth.models import User
 
 
