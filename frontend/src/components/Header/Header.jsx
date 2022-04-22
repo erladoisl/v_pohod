b0ac66a7f11bfbd4100dfc682af7526e699795b3
Rakhina Latypova <rakhina.bdd@gmail.com>
2022-04-22 16:08:25 +0300

    поправлены названия вкладок

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -21,13 +21,8 @@ function Header() {
           <div className="collapse navbar-collapse" id="navbarCollapse">
             <ul className="navbar-nav me-auto mb-2 mb-md-0">
               <li className="nav-item">
-                <NavLink to='/raskladki' className="nav-link ">
-                  Главная
-                </NavLink>
-              </li>
-              <li className="nav-item">
                 <NavLink to='/hikes' className="nav-link ">
-                  Архив Раскладок
+                  Походы
                 </NavLink>
               </li>
               <li className="nav-item">
