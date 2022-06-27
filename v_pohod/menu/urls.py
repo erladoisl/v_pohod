99f36c7541cbdae3588d950ed7ff3b91df2a3265
Rakhina Latypova <rakhina.bdd@gmail.com>
2022-06-27 15:45:20 +0300

    Даны названия ссылкам

--- v_pohod/menu/urls.py
+++ v_pohod/menu/urls.py
@@ -7,10 +7,10 @@ from menu.views import IngredientView
 from menu.views import XlsxView
 
 urlpatterns = [
-    path('api/eating-category/', EatingCategoryView.as_view(), name='get_eating_categories'),
-    path('api/food/', FoodView.as_view()),
-    path('api/formula/', FormulaView.as_view()),
-    path('api/hike/day/eatings/', EatingView.as_view()), # получение списка приемов пищи за день, добавление, изменение, удаление
+    path('api/eating-category/', EatingCategoryView.as_view(), name='eating_categories'),
+    path('api/food/', FoodView.as_view(), name='food'),
+    path('api/formula/', FormulaView.as_view(), name='formula'),
+    path('api/hike/day/eatings/', EatingView.as_view(), name='eating'), # получение списка приемов пищи за день, добавление, изменение, удаление
     path('api/hike/day/eating/ingredient/', IngredientView.as_view()), # получение списка игредиентов за прием пищи, добавление, изменение, удаление
     path('api/hike/menu/xlsx/', XlsxView.as_view())
 ]
