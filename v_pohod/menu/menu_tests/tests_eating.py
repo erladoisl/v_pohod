189d13ac3de95b82075c28e326f2f4fa565a9270
Rakhina Latypova <rakhina.bdd@gmail.com>
2022-06-28 08:31:29 +0300

    eating tests

--- v_pohod/menu/menu_tests/tests_eating.py
+++ v_pohod/menu/menu_tests/tests_eating.py
@@ -1,4 +1,3 @@
-import json
 from rest_framework.test import APITestCase, APIClient
 from django.urls import reverse
 from rest_framework import status
@@ -8,39 +7,43 @@ from menu.menu_tests.model_factory import FormulaFactory
 from hike.hike_tests.model_factory import HikeDayFactory, HikeFactory
 from menu.menu_tests.model_factory import EatingCategoryFactory, EatingFactory
 from menu.models import Eating
+from user.tests.model_factory import AdminFactory
 
 
-class FoodTestCase(APITestCase):
+class EatingTestCase(APITestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         cls.authorized_user = UserFactory.create()
         cls.user = UserFactory.create()
+        cls.admin = AdminFactory.create()
         cls.auth_user_hike = HikeFactory(user=cls.authorized_user)
 
         cls.auth_user_hike_days = [HikeDayFactory(hike=cls.auth_user_hike),
                                    HikeDayFactory(hike=cls.auth_user_hike),
                                    HikeDayFactory(hike=cls.auth_user_hike)]
 
-        cls.eating_categories = [EatingCategoryFactory(name='Завтрак'), 
-                                 EatingCategoryFactory(name='Обед'), 
+        cls.eating_categories = [EatingCategoryFactory(name='Завтрак'),
+                                 EatingCategoryFactory(name='Обед'),
                                  EatingCategoryFactory(name='Ужин')]
 
-        cls.food = [FoodFactory(name='Греча', unit='гр.'), 
-                    FoodFactory(name='Тушенка', unit='шт.'), 
+        cls.food = [FoodFactory(name='Греча', unit='гр.'),
+                    FoodFactory(name='Тушенка', unit='шт.'),
                     FoodFactory(name='Хлеб', unit='гр.')]
 
         cls.formula = [FormulaFactory(name='1 шт. на 4 порции', value='ceil(1 / 4 * PARTICIPANT_COUNT)'),
-                       FormulaFactory(name='1 шт. на поход',value='1 / TOTAL_COUNT'),
+                       FormulaFactory(name='1 шт. на поход',
+                                      value='1 / TOTAL_COUNT'),
                        FormulaFactory(name='1 шт. на прием пищи', value='1')]
 
-        cls.eatings = [EatingFactory(hikeDay=cls.auth_user_hike_days[0], eating_category=cls.eating_categories[1]), 
-                       EatingFactory(hikeDay=cls.auth_user_hike_days[0], eating_category=cls.eating_categories[2]), 
+        cls.eatings = [EatingFactory(hikeDay=cls.auth_user_hike_days[0], eating_category=cls.eating_categories[1]),
+                       EatingFactory(
+                           hikeDay=cls.auth_user_hike_days[0], eating_category=cls.eating_categories[2]),
                        EatingFactory(hikeDay=cls.auth_user_hike_days[1], eating_category=cls.eating_categories[0])]
         cls.client = APIClient()
         cls.url_eating = reverse('eating')
 
-    def test_getting_all_eating_by_hike_day_success(self):
+    def test_getting_all_eating_success_by_hike_day(self):
         self.client.force_authenticate(user=self.authorized_user)
 
         response = self.client.get(
@@ -52,9 +55,10 @@ class FoodTestCase(APITestCase):
 
         for response_eating, eating in zip(eatings, self.eatings):
             self.assertEqual(response_eating['name'], eating.name)
-            self.assertEqual(response_eating['description'], eating.description)
+            self.assertEqual(
+                response_eating['description'], eating.description)
 
-    def test_getting_all_eating_by_hike_day_unauthorised(self):
+    def test_getting_all_eating_error_by_hike_day_unauthorised(self):
         response = self.client.get(self.url_eating)
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
@@ -83,99 +87,188 @@ class FoodTestCase(APITestCase):
         self.assertNotEqual(eating.description, eating_fields['description'])
         self.assertNotEqual(eating.number, eating_fields['number'])
 
-    # def test_edit_formula_success(self):
-    #     self.client.force_authenticate(user=self.authorized_user)
-    #     formula_fields = {
-    #         'id': self.formula[0].pk,
-    #         'name': 'new name',
-    #         'value': 'new value',
-    #     }
-    #     response = self.client.post(
-    #         self.url_formula, formula_fields)
+    def test_edit_eating_success_by_owner(self):
+        self.client.force_authenticate(user=self.authorized_user)
+        eating_fields = {
+            'id': self.eatings[0].pk,
+            'name': 'new name',
+            'description': 'new descr',
+            'eating_category_id': self.eating_categories[2].pk,
+            'hikeDay_id': self.eatings[0].hikeDay.pk,
+            'number': 0,
+        }
 
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     self.assertEqual(
-    #         response.data, {'error': False, 'message': 'Изменения сохранены'})
+        response = self.client.post(
+            self.url_eating, eating_fields)
 
-    #     formula = Formula.objects.get(pk=formula_fields['id'])
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data, {'error': False, 'message': 'Успешно сохранено'})
 
-    #     self.assertEqual(formula.name, formula_fields['name'])
+        eating = Eating.objects.get(pk=eating_fields['id'])
 
-    # def test_edit_formula_error_unauthorised(self):
-    #     response = self.client.post(self.url_formula)
+        self.assertEqual(eating.name, eating_fields['name'])
+        self.assertEqual(eating.description, eating_fields['description'])
+        self.assertEqual(eating.eating_category.pk,
+                         eating_fields['eating_category_id'])
+        self.assertEqual(eating.number, eating_fields['number'])
 
-    #     self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+    def test_edit_eating_success_by_admin(self):
+        self.client.force_authenticate(user=self.admin)
+        eating_fields = {
+            'id': self.eatings[0].pk,
+            'name': 'new name',
+            'description': 'new descr',
+            'eating_category_id': self.eating_categories[2].pk,
+            'hikeDay_id': self.eatings[0].hikeDay.pk,
+            'number': 0,
+        }
 
-    # def test_delete_formula_success(self):
-    #     self.client.force_authenticate(user=self.authorized_user)
-    #     formula_fields = {
-    #         'name': self.formula[0].name,
-    #     }
+        response = self.client.post(
+            self.url_eating, eating_fields)
 
-    #     self.assertEqual(len(Formula.objects.filter(
-    #         name=formula_fields['name'])), 1)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data, {'error': False, 'message': 'Успешно сохранено'})
 
-    #     self.assertEqual(len(Formula.objects.filter(name=formula_fields['name'])), 1)
-    #     response = self.client.delete(self.url_formula, formula_fields)
+        eating = Eating.objects.get(pk=eating_fields['id'])
 
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     self.assertEqual(
-    #         response.data, {'error': False, 'message': 'Успешно удалено'})
-    #     self.assertEqual(len(Formula.objects.filter(name=formula_fields['name'])), 0)
+        self.assertEqual(eating.name, eating_fields['name'])
+        self.assertEqual(eating.description, eating_fields['description'])
+        self.assertEqual(eating.eating_category.pk,
+                         eating_fields['eating_category_id'])
+        self.assertEqual(eating.number, eating_fields['number'])
 
-    # def test_delete_formula_error_unauthorised(self):
-    #     self.assertEqual(len(Formula.objects.filter()), 3)
-    #     response = self.client.delete(self.url_formula, {
-    #                                   'id': self.formula[0].pk})
+    def test_edit_eating_error_unauthorised(self):
+        response = self.client.post(self.url_eating)
 
-    #     self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-    #     self.assertEqual(len(Formula.objects.filter()), 3)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    # def test_add_formula_error_not_uniq_name(self):
-    #     self.client.force_authenticate(user=self.authorized_user)
-    #     formula_fields = {
-    #         'name': self.formula[0].name,
-    #         'value': 'new value',
-    #     }
+    def test_delete_eating_success_by_owner(self):
+        self.client.force_authenticate(user=self.authorized_user)
+        eating_id = self.eatings[0].pk,
 
-    #     self.assertEqual(len(Formula.objects.filter()), 3)
+        self.assertEqual(len(Eating.objects.filter(pk=self.eatings[0].pk)), 1)
 
-    #     response = self.client.post(
-    #         self.url_formula, formula_fields)
+        response = self.client.delete(self.url_eating, {'id': eating_id})
 
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     self.assertEqual(
-    #         response.data, {'error': True, 'message': 'Название не уникальное'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data, {'error': False, 'message': 'Успешно удалено'})
+        self.assertEqual(len(Eating.objects.filter(pk=self.eatings[0].pk)), 0)
 
-    #     self.assertEqual(len(Formula.objects.filter()), 3)
+    def test_delete_eating_error_unauthorised(self):
+        self.assertEqual(len(Eating.objects.filter()), 3)
+        response = self.client.delete(self.url_eating, {'id': self.formula[0].pk})
 
-    # def test_add_formula_success(self):
-    #     self.client.force_authenticate(user=self.authorized_user)
-    #     formula_fields = {
-    #         'name': 'new name',
-    #         'value': 'new value',
-    #     }
-    #     self.assertEqual(len(Formula.objects.filter()), 3)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(len(Eating.objects.filter()), 3)
 
-    #     response = self.client.post(
-    #         self.url_formula, formula_fields)
+    def test_delete_eating_success_by_admin(self):
+        self.client.force_authenticate(user=self.admin)
+        eating_id = self.eatings[0].pk,
 
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     self.assertEqual(
-    #         response.data, {'error': False, 'message': 'Успешно добавлена формула'})
-            
-    #     formula = Formula.objects.get(name=formula_fields['name'])
+        self.assertEqual(len(Eating.objects.filter(pk=self.eatings[0].pk)), 1)
 
-    #     self.assertEqual(len(Formula.objects.filter()), 4)
-    #     self.assertEqual(formula.name, formula_fields['name'])
-    #     self.assertEqual(formula.value, formula_fields['value'])
+        response = self.client.delete(self.url_eating, {'id': eating_id})
 
-    # def test_add_formula_error_unauthorised(self):
-    #     formula_fields = {
-    #         'name': 'new name',
-    #         'value': 'new value',
-    #     }
-    #     response = self.client.post(
-    #         self.url_formula, formula_fields)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data, {'error': False, 'message': 'Успешно удалено'})
+        self.assertEqual(len(Eating.objects.filter(pk=self.eatings[0].pk)), 0)
 
-    #     self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+    def test_delete_eating_error_by_not_owner_and_no_admin(self):
+        self.client.force_authenticate(user=self.user)
+        self.assertEqual(len(Eating.objects.filter()), 3)
+        response = self.client.delete(self.url_eating, {'id': self.formula[0].pk})
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data, {'error': True, 'message': 'Нельзя редактировать чужие походы'})
+        self.assertEqual(len(Eating.objects.filter(pk=self.eatings[0].pk)), 1)
+
+    def test_add_eating_error_by_not_owner_and_not_admin(self):
+        self.client.force_authenticate(user=self.user)
+        eating_fields = {
+            'name': 'new name',
+            'description': 'new descr',
+            'eating_category_id': self.eating_categories[2].pk,
+            'hikeDay_id': self.eatings[0].hikeDay.pk,
+            'number': 0,
+        }
+
+        self.assertEqual(len(Eating.objects.filter()), 3)
+
+        response = self.client.post(
+            self.url_eating, eating_fields)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data, {'error': True, 'message': 'Нельзя редактировать чужие походы'})
+
+        self.assertEqual(len(Eating.objects.filter()), 3)
+
+    def test_add_eating_success_by_owner(self):
+        self.client.force_authenticate(user=self.authorized_user)
+        eating_fields = {
+            'name': 'new name',
+            'description': 'new descr',
+            'eating_category_id': self.eating_categories[2].pk,
+            'hikeDay_id': self.eatings[0].hikeDay.pk,
+            'number': 0,
+        }
+
+        response = self.client.post(
+            self.url_eating, eating_fields)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data, {'error': False, 'message': 'Прием пищи создан успешно', 'id': 4})
+
+        eating = Eating.objects.get(pk=response.data['id'])
+
+        self.assertEqual(len(Eating.objects.filter()), 4)
+        self.assertEqual(eating.name, eating_fields['name'])
+        self.assertEqual(eating.description, eating_fields['description'])
+        self.assertEqual(eating.eating_category.pk,
+                         eating_fields['eating_category_id'])
+        self.assertEqual(eating.number, eating_fields['number'])
+
+    def test_add_eating_success_by_admin(self):
+        self.client.force_authenticate(user=self.admin)
+        eating_fields = {
+            'name': 'new name',
+            'description': 'new descr',
+            'eating_category_id': self.eating_categories[2].pk,
+            'hikeDay_id': self.eatings[0].hikeDay.pk,
+            'number': 0,
+        }
+
+        response = self.client.post(
+            self.url_eating, eating_fields)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data, {'error': False, 'message': 'Прием пищи создан успешно', 'id': 4})
+
+        eating = Eating.objects.get(pk=response.data['id'])
+
+        self.assertEqual(len(Eating.objects.filter()), 4)
+        self.assertEqual(eating.name, eating_fields['name'])
+        self.assertEqual(eating.description, eating_fields['description'])
+        self.assertEqual(eating.eating_category.pk,
+                         eating_fields['eating_category_id'])
+        self.assertEqual(eating.number, eating_fields['number'])
+
+    def test_add_eating_error_unauthorised(self):
+        eating_fields = {
+            'name': 'new name',
+            'description': 'new descr',
+            'eating_category_id': self.eating_categories[2].pk,
+            'hikeDay_id': self.eatings[0].hikeDay.pk,
+            'number': 0,
+        }
+
+        response = self.client.post(self.url_eating, eating_fields)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
