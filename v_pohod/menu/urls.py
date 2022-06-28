94ebf255a24e8b3ec45d3eadd4d707d9c4a7e02c
Rakhina Latypova <rakhina.bdd@gmail.com>
2022-06-28 13:36:01 +0300

    + название ссылки ingredient

--- v_pohod/menu/urls.py
+++ v_pohod/menu/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('api/food/', FoodView.as_view(), name='food'),
     path('api/formula/', FormulaView.as_view(), name='formula'),
     path('api/hike/day/eatings/', EatingView.as_view(), name='eating'), # получение списка приемов пищи за день, добавление, изменение, удаление
-    path('api/hike/day/eating/ingredient/', IngredientView.as_view()), # получение списка игредиентов за прием пищи, добавление, изменение, удаление
+    path('api/hike/day/eating/ingredient/', IngredientView.as_view(), name='ingredient'), # получение списка игредиентов за прием пищи, добавление, изменение, удаление
     path('api/hike/menu/xlsx/', XlsxView.as_view())
 ]
