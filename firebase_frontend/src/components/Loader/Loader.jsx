173d3b661d0645c53e66aa6a58b12cabab2af7c3
NewSafePass42 <rakhina@bdd.local>
2022-11-10 12:47:43 +0300

    bf

--- firebase_frontend/src/components/Loader/Loader.jsx
+++ firebase_frontend/src/components/Loader/Loader.jsx
@@ -3,7 +3,7 @@ import c from './Loader.module.css';
 const Loader = (() => {
     return (
         <div className={c.back}>
-            <div class={c.loader}>
+            <div className={c.loader}>
                 <div className="spinner-border text-primary" role="status">
                     <span className="visually-hidden">Loading...</span>
                 </div>
