dfdd7cde3c80c2241848c86e3b1191f32d660950
erladoisl <erladoisl@gmail.com>
2023-03-09 13:28:12 +0300

    bf: eating_category_id default value

--- firebase_frontend/src/components/Eatings/Eatings.jsx
+++ firebase_frontend/src/components/Eatings/Eatings.jsx
@@ -110,7 +110,7 @@ const Eatings = ((props) => {
 
             <div className='text-start align-self-end'>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-circle mx-1" viewBox="0 0 16 16" style={{ cursor: 'pointer' }}
-                    onClick={(() => { updateEating({ hike_day_id: props.day_id }) })}>
+                    onClick={(() => { updateEating({ hike_day_id: props.day_id,  eating_category_id: "1"}) })}>
                     <title> Добавить прием пищи </title>
                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
                     <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
